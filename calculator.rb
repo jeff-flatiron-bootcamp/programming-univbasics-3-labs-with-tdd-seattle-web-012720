40186434fada464476886c17fcfee4961f1c4ac9
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-04 22:49:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,5 @@ first_number = 1
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
-product = first_number * second_number
+produc = first_number * second_number
 quotien = first_number / second_number
